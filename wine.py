82334b391d9dbc46bb2afcfce6f025ab2dcfda25
wesdsfv <144088464+wesdsfv@users.noreply.github.com>
2024-09-13 16:38:21 +0800

    Update wine.py

--- wine.py
+++ wine.py
@@ -7,7 +7,7 @@ import os
 
 # Load the trained KMeans model and the scaler
 # Note: Adjust the paths accordingly to where your model and scaler are saved
-model_path = "./wine/kmeans_wine_clustering.joblib"
+model_path = "./main/kmeans_wine_clustering.joblib"
 model = joblib.load(model_path)
 
 # Streamlit title and description
